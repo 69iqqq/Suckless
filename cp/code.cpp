f7ac098f0a1f06aae26bf79073d2eec8110b78ef
Abir Dasgupta <abirdasgupta09@gmail.com>
2024-09-30 14:29:56 +0530

    Add files via upload

---
+++ cp/code.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+
+  int a, b;
+  cin >> a >> b;
+  int c = a + b;
+  cout << c << endl;
+  cout << "hello my boy" << endl;
+  cout << "hello: " << endl;
+}
+int main() {
+  cin.tie(0);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
